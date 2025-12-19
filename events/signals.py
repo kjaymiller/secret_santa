db1719957aa361d82aa8cb9674ff2dbb448444e1
Jay Miller <kjaymiller@gmail.com>
2025-12-19 11:35:28 -0500

    feat: Implement account creation email notification

--- events/signals.py
+++ events/signals.py
@@ -3,12 +3,15 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from .models import UserProfile
+from .services.notifications import get_notification_service
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
+        # Send welcome email
+        get_notification_service().send_account_creation_email(instance)
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
