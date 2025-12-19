0fa11dbe6f335f0e483ec519d68ce7c46a4b3e8e
Jay Miller <kjaymiller@gmail.com>
2025-12-18 21:33:24 -0500

    Feat(internal): Add styleguide (debug only)

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 from events.views import HomeView
 
@@ -14,4 +15,7 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    urlpatterns += [
+        path("styleguide/", TemplateView.as_view(template_name="styleguide.html"), name="styleguide"),
+    ]
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
